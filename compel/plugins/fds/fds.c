79b4f507a34360309d3df0e37d1aa104df0934dc
Cyrill Gorcunov <gorcunov@openvz.org>
2016-11-14 23:01:17 +0300

    compel: plugins,fds -- Unify prototypes
     - Declare send_fds, recv_fds in sch.h, these
       are prototypes used in both compel and criu
     - Drop old protos from plugin-fds.h uapi file
     - Drop old code from fds.c source
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- compel/plugins/fds/fds.c
+++ compel/plugins/fds/fds.c
@@ -15,23 +15,6 @@
 #define __sys(foo)	sys_##foo
 #define __memcpy	std_memcpy
 
-#ifdef SCM_FDSET_HAS_OPTS
-#error "Spurious SCM_FDSET_HAS_OPTS"
-#endif
-
-#define SCM_FDSET_HAS_OPTS
-
-#include "common/scm.h"
 #include "common/scm-code.c"
-
-int fds_send(int *fds, int nr_fds)
-{
-	return 0; // fds_send_via(std_ctl_sock(), fds, nr_fds);
-}
-
-int fds_recv(int *fds, int nr_fds)
-{
-	return -1; //fds_recv_via(std_ctl_sock(), fds, nr_fds);
-}
 
 PLUGIN_REGISTER_DUMMY(fds)
--- compel/plugins/include/uapi/plugin-fds.h
+++ compel/plugins/include/uapi/plugin-fds.h
@@ -5,30 +5,9 @@
 #ifndef __COMPEL_PLUGIN_FDS_H__
 #define __COMPEL_PLUGIN_FDS_H__
 
-#include <sys/socket.h>
 #include <sys/un.h>
 
-extern int fds_send(int *fds, int nr_fds);
-extern int fds_recv(int *fds, int nr_fds);
-
-static inline int fds_send_one(int fd)
-{
-	return fds_send(&fd, 1);
-}
-
-static inline int fds_recv_one(void)
-{
-	int fd, ret;
-
-	ret = fds_recv(&fd, 1);
-	if (ret)
-		fd = -1;
-
-	return fd;
-}
-
-extern int send_fds(int sock, struct sockaddr_un *saddr, int len,
-		    int *fds, int nr_fds, bool with_flags);
+#include "common/scm.h"
 
 static inline int send_fd(int sock, struct sockaddr_un *saddr, int saddr_len, int fd)
 {
